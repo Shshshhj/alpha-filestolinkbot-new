64b36352e258227a0d8d2952cf7ca49471cde523
SOORAJ S R <rsoorajs@gmail.com>
2019-10-20 16:22:56 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -53,8 +53,7 @@ async def get_link(bot, update):
     if update.from_user.id not in users:
         users.append(update.from_user.id)
         
-    else:pass
-       
+     else:
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.ABS_TEXT,
