1677191c07c6f221190538391b5b71496b330082
SOORAJ S R <rsoorajs@gmail.com>
2019-10-20 16:42:57 +0530

    Update progress.py

--- tools/progress.py
+++ tools/progress.py
@@ -15,7 +15,7 @@ import time
 async def progress_for_pyrogram(current, total, client, ud_type, message_id, chat_id, start):
     now = time.time()
     diff = now - start
-    if round(diff % 5.00) == 0 or current == total:
+    if round(diff % 25.00) == 0 or current == total:
         # if round(current / total * 100, 0) % 5 == 0:
         percentage = current * 100 / total
         speed = current / diff
