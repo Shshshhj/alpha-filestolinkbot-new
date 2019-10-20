70b5df1642f20ef858aec4f08dad958a2ed76832
Charuka <sljohnwick@gmail.com>
2019-10-20 12:39:21 +0530

    fix

--- tools/translation.py
+++ tools/translation.py
@@ -1,17 +1,14 @@
 class Translation(object):
     START_TEXT = """
 👉 Forward any files to this bot,and bot will generate its directlink.
-👉 If you dont get directlink after 1 hour, forward that file again to the bot
 👉 Subscribe our channel for bot updates @filestolink
-👉 Direct links are only for personal use, do not share with others. We are not responsible for any content that you generates direct links.
-
-© Source Code : [SpEcHlDe](https://github.com/SpEcHiDe/AnyDLBot)
     """
     HELP_USER = """
 👉 Forward any files to this bot,and bot will generate its directlink.
 👉 If you dont get directlink after 1 hour, forward that file again to the bot
-👉 Subscribe our channel for bot updates @filestolink
-👉 Direct links are only for personal use, do not share with others.we are not responsible for any content that you generates direct links.
+👉 Direct links are only for personal use, do not share with others. We are not responsible for any content that you generates direct links.
+
+© Source Code : [SpEcHlDe](https://github.com/SpEcHiDe/AnyDLBot)
     """
     ABS_TEXT = "Please don't be selfish."
     DOWNLOAD_START = "📤 Your request is in the queue. Please be patient..."
